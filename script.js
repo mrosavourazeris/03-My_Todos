bbb62529ff6ad9562ef68add4c67fe54c915e14a
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2020-11-28 11:55:11 -0600

    Created a function to render the todos into a list in the browser

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+//04-Stu_Render_Todos START
+function renderToDo (event){
+    for ( var i = 0; i < todos.length; i++){
+        //create li element
+        var li = document.createElement("li")
+        //add todo text to li 
+        li.innerText = todos[i]
+        //append li to todoList
+        todoList.appendChild(li)
+    }
+    todoCountSpan.innerText = todos.length
+}
+
+renderToDo()
+
+//04-Stu_Render_Todos END
